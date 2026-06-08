6bc9a95a1d5e2dd640c943a8af0bc4da729413ba
unknown <Abduvoris lox>
2026-06-08 10:21:08 +0500

    NextJs ishladim

---
+++ Components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className='h-[80px] flex justify-center gap-3 bg-amber-500 text-[20px]'>
+        <ul className='flex justify-center gap-3'> 
+            <li>
+                <Link href="/">Home</Link>
+            </li>
+            <li>
+                <Link href="/Register/signUp">SignUp</Link>
+            </li>
+            <li>
+                <Link href="/signIn">SignIn</Link>
+            </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ pages/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Head from 'next/head'
+
+import Navbar from '@/Components/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+      </Head>
+
+      
+      <h1 className='text-center mt-7 text-2xl'> <i className="text-blue-500 fa-solid fa-diamond"></i> Salom page Home</h1>
+  
+    </div>
+  )
+}
+
+export default Home
